ff739bbb4180efa947e3d5ffd73f73844a7aaa79
Christian Zema <chriszeymar@gmail.com>
2022-06-17 17:11:44 +0200

    Push existing project to GitLab

--- README.md
+++ README.md
@@ -1,14 +1,10 @@
-# Latihan CRUD Dengan Cubit
+
 
 ## Getting Started
 
-Latihan Dasar update UI menggunakan Cubit (Bloc) dan Equitable ,memanfaatkan
-teknik copyWith untuk update nilai pada State supaya menghasilkan
-state yg berbeda dengan sebelumnya agar UI Widget terupdate oleh Bloc Builder
+Application created using Flutter version 2.10.5
 
-Aplikasi dibuat menggunakan Flutter versi 2.10.5
-
-Selamat Mempelajari dan semoga bermanfaat :D
+Happy learning and hopefully useful :D
 
 
  ![](images/home2.jpeg)
--- lib/database.dart
+++ lib/database.dart
@@ -27,4 +27,44 @@ static Future<void> addItem({
       .whenComplete(() => print("Notes item added to the database"))
       .catchError((e) => print(e));
 }
+static Stream<QuerySnapshot<Object?>> readItems() {
+  CollectionReference clientsItemCollection =
+      _mainCollection.doc(clientId).collection('clients');
+
+  return clientsItemCollection.snapshots();
+}
+
+
+static Future<void> deleteItem({
+  required String docId,
+}) async {
+  DocumentReference documentReferencer =
+      _mainCollection.doc(clientId);
+
+  await documentReferencer
+      .delete()
+      .whenComplete(() => print('Note item deleted from the database'))
+      .catchError((e) => print(e));
+}
+static Future<void> updateItem({
+  required String description,
+  required String clientId,
+  required var quantity,
+  required var itemPrice,
+}) async {
+  DocumentReference documentReferencer =
+      _mainCollection.doc(clientId);
+
+  Map<String, dynamic> data = <String, dynamic>{
+    "description": description,
+    "quantity": quantity,
+    "itemprice": itemPrice,
+  };
+
+  await documentReferencer
+      .update(data)
+      .whenComplete(() => print("Note item updated in the database"))
+      .catchError((e) => print(e));
+}
+
 }
